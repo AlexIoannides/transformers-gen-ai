3af754d185f2e2aa2c60983523703dbac4479ae9
alexioannides <alex.ioanndies@yahoo.co.uk>
2023-05-06 11:05:22 +0100

    Add signatures for data preprocessing

--- modelling/data.py
+++ modelling/data.py
@@ -1,10 +1,14 @@
-"""Downloading and processing IMDB film review data."""
+"""Downloading and pre-processing IMDB film review data."""
+import re
 import warnings
+from collections import Counter, OrderedDict
 from pathlib import Path
 from typing import Tuple
 
 from pandas import DataFrame
+from torch import tensor
 from torchtext.datasets import IMDB
+from torch.utils.data import Dataset
 
 TORCH_DATA_CACHE_PATH = Path(".data")
 
@@ -26,3 +30,36 @@ def get_data() -> Tuple[DataFrame, DataFrame]:
     test_data["sentiment"] = test_data["sentiment"].apply(lambda e: e - 1)
 
     return train_data, test_data
+
+
+class FilmReviewDataset(Dataset):
+    """IMDB film review dataset."""
+
+    def __init__(data: DataFrame):
+        pass
+
+    def __len__(self) -> int:
+        pass
+
+    def __getitem__(self, idx: int) -> Tuple[int, str]:
+        pass
+
+
+class BasePreProcessor:
+    """Basic pre-processor for use as a collate_fn for DataLoaders."""
+
+    def __init__(self, train_data: Dataset):
+        pass
+
+    def __call__(self, instance: Tuple[int, str]) -> Tuple[tensor, tensor, tensor]:
+        pass
+
+    def _tokenizer(text: str) -> str:
+        text = re.sub(r"<[^>]*>", "", text)
+        emoticons = re.findall(
+            r"(?::|;|=)(?:-)?(?:\)|\(|D|P)", text.lower()
+        )
+        text = re.sub(r"[\W]+", " ", text.lower()) +\
+            " ".join(emoticons).replace("-", "")
+        tokenized = text.split()
+        return tokenized
