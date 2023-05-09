f5df0a306e151a229bd7fdf8493c011c89db87f6
alexioannides <alex.ioanndies@yahoo.co.uk>
2023-05-09 15:28:31 +0100

    Implement custom tokenizer class

--- modelling/data.py
+++ modelling/data.py
@@ -34,8 +34,8 @@ def get_data() -> Tuple[DataFrame, DataFrame]:
     return train_data, test_data
 
 
-class FilmReviewDataset(Dataset):
-    """IMDB film review dataset."""
+class FilmReviewSentiment(Dataset):
+    """IMDB film reviews and associated sentiment."""
 
     def __init__(self, split: str = "train"):
         if split not in ("train", "test", "all"):
@@ -63,11 +63,28 @@ class FilmReviewDataset(Dataset):
 class BasePreprocessor:
     """Basic pre-processor for use as a collate_fn for DataLoaders."""
 
-    def __init__(self, train_data: Dataset):
-        token_counter = Counter()
-        for x, _ in train_data:
-            token_counter.update(self._tokenize(x))
+    def __init__(self):
+        self._tokenizer = IMDBTokenizer()
+
+    def __call__(self, batch: List[Tuple[str, int]]) -> Tuple[tensor, tensor, tensor]:
+        y = [tensor(e) for _, e in batch]
+        x = [tensor(self._tokenizer(review)) for review, _ in batch]
+        x_padded = pad_sequence(x, batch_first=True)
+        x_lens = [e.shape[0] for e in x]
+        return x_padded, y, x_lens
+
+
+class IMDBTokenizer:
+    """Word to integer tokenisation for use with any dataset or model."""
+
+    def __init__(self):
+        train_and_test = concat(get_data(), ignore_index=True)
+        reviews = train_and_test["review"].tolist()
+        reviews = re.sub(r"[\.\?](\s|$)", " endofsentence ", " ".join(reviews))
+
+        token_counter = Counter(self._tokenize(reviews))
         token_freqs = sorted(token_counter.items(), key=lambda e: e[1], reverse=True)
+
         _vocab = vocab(OrderedDict(token_freqs))
         _vocab.insert_token("<pad>", 0)
         _vocab.insert_token("<unk>", 1)
@@ -75,16 +92,12 @@ class BasePreprocessor:
         self.vocab = _vocab
         self.vocab_size = len(self.vocab)
 
-    def __call__(self, batch: List[Tuple[str, int]]) -> Tuple[tensor, tensor, tensor]:
-        y = [tensor(e) for _, e in batch]
-        tokens = [self._tokenize(review) for review, _ in batch]
-        x = [tensor(self.vocab(token)) for token in tokens]
-        x_padded = pad_sequence(x, batch_first=True)
-        x_lens = [e.shape[0] for e in x]
-        return x_padded, y, x_lens
+    def __call__(self, text: str) -> List[int]:
+        return [self.vocab(token) for token in self._tokenize(text)]
 
     @staticmethod
     def _tokenize(text: str) -> List[str]:
+        """Basic tokenizer that can strip HTML."""
         text = re.sub(r"<[^>]*>", "", text)
         emoticons = re.findall(
             r"(?::|;|=)(?:-)?(?:\)|\(|D|P)", text.lower()
@@ -96,6 +109,6 @@ class BasePreprocessor:
 
 
 if __name__ == "__main__":
-    train_data = FilmReviewDataset("train")
+    train_data = FilmReviewSentiment("train")
     preproc = BasePreprocessor(train_data)
     train_data_loader = DataLoader(train_data, batch_size=4, collate_fn=preproc)
