289501b1601dbcccd1a426435707e6d647f5c5c0
alexioannides <alex.ioanndies@yahoo.co.uk>
2023-05-19 22:02:39 +0100

    Refactor location of LSTM text generator

--- modelling/lstm.py
+++ modelling/lstm.py
@@ -3,10 +3,12 @@ from datetime import datetime
 from typing import Tuple
 
 from torch import device, manual_seed, tensor, Tensor, zeros
+from torch.distributions import Categorical
 from torch.nn import CrossEntropyLoss, Embedding, Linear, Module, LSTM
 from torch.utils.data import DataLoader
 from torch.optim import Adam
 
+from .data import _Tokenizer
 from .utils import get_device
 
 
@@ -75,7 +77,38 @@ def train_next_word_prediction(
     return loss.item() / sequence_lenth
 
 
+def generate(
+    model: NextWordPrediction,
+    prompt: str,
+    tokenizer: _Tokenizer,
+    output_length: int = 40,
+    temperature: float = 1.0,
+    random_seed: int = 42,
+    device_: device = device("cpu"),
+) -> str:
+    """Generate new text conditional on a text prompt."""
+    manual_seed(random_seed)
+
+    model.to(device_)
+    model.eval()
+    hidden, cell = model.initialise(1, device_)
+
+    prompt_tokens = tensor(tokenizer(prompt), device=device_).reshape(-1, 1)
+    for token in prompt_tokens[:-1]:
+        _, hidden, cell = model(token, hidden, cell)
+
+    new_token_sequence = [prompt_tokens[-1]]
+    for _ in range(output_length):
+        token_logits, hidden, cell = model(new_token_sequence[-1], hidden, cell)
+        token_pred = Categorical(logits=temperature * token_logits).sample()
+        new_token_sequence += [token_pred]
+
+    new_text = prompt + " " + " ".join(tokenizer.tokens2text(new_token_sequence[1:]))
+    return new_text.replace(" endofsentence ", ". ")
+
+
 if __name__ == "__main__":
+    # train model
     from .data import FilmReviewSequences
     from .utils import save_model
 
@@ -94,3 +127,16 @@ if __name__ == "__main__":
     model = NextWordPrediction(data.vocab_size, SIZE_EMBED, SIZE_HIDDEN)
     model_loss = train_next_word_prediction(model, data_loader, EPOCHS, LEARNING_RATE)
     save_model(model, name=MODEL_NAME, loss=model_loss)
+
+    # generate text
+    from .data import IMDBTokenizer
+    from .utils import load_model
+
+    MODEL_NAME = "lstm_next_word_gen"
+    PROMPT = "This movie was a total waste of time"
+    TEMPERATURE = 1.0
+
+    tokenizer = IMDBTokenizer()
+    model: NextWordPrediction = load_model(MODEL_NAME)
+    new_text = generate(model, PROMPT, tokenizer, temperature=TEMPERATURE)
+    print(new_text)
