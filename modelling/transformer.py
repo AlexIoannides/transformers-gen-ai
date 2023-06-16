d4786c9c93cd92b61b56e110f93189f4cc13de98
alexioannides <alex.ioanndies@yahoo.co.uk>
2023-06-16 23:37:00 +0100

    Add learning rate warmup to transformer

--- modelling/transformer.py
+++ modelling/transformer.py
@@ -1,6 +1,9 @@
 """Language modelling using multi-head attention transformers."""
 from __future__ import annotations
+
+import math
 from datetime import datetime
+from functools import partial
 from typing import Dict, Tuple
 
 from torch import (
@@ -31,6 +34,7 @@ from torch.nn.init import xavier_uniform_
 from torch.nn.utils import clip_grad_norm_
 from torch.utils.data import DataLoader
 from torch.optim import Adam
+from torch.optim.lr_scheduler import LambdaLR
 from tqdm import tqdm
 
 from .data import _Tokenizer, EOS_DELIM, PAD_TOKEN_IDX
@@ -47,7 +51,9 @@ class NextWordPredictionTransformer(Module):
         self._n_heads = n_heads
         self._position_encoder = PositionalEncoding(size_embed)
         self._embedding = Embedding(size_vocab, size_embed)
-        self._decoder = TransformerDecoderLayer(size_embed, n_heads, batch_first=True)
+        self._decoder = TransformerDecoderLayer(
+            size_embed, n_heads, dim_feedforward=2*size_embed, batch_first=True
+        )
         self._linear = Linear(size_embed, size_vocab)
         self._init_weights()
 
@@ -93,7 +99,7 @@ class PositionalEncoding(Module):
         pos_encoding = zeros(max_seq_len, size_embed)
         pos_encoding[:, 0::2] = sin(position * div_term)
         pos_encoding[:, 1::2] = cos(position * div_term)
-        self.register_buffer('pos_encoding', pos_encoding)  # don't train these
+        self.register_buffer('_pos_encoding', pos_encoding)  # don't train these
 
     def forward(self, x: Tensor) -> Tensor:
         """
@@ -101,8 +107,16 @@ class PositionalEncoding(Module):
             x: Tensor, shape ``[batch_size, seq_len, embedding_dim]``
         """
         seq_len = x.size(1)
-        x = x + self.pos_encoding[:seq_len]
+        x = x + self._pos_encoding[:seq_len]
         return self._dropout(x)
+
+
+def cosine_warmup_schedule(step: int, warmup_steps: int, max_steps: int):
+    """Learning rate schedule function taken from Hugging Face."""
+    lr_factor = 0.5 * (1 + math.cos(math.pi * step / max_steps))
+    if step <= warmup_steps:
+        lr_factor *= step / warmup_steps
+    return lr_factor
 
 
 def train(
@@ -110,6 +124,8 @@ def train(
     sequence_data: DataLoader,
     n_epochs: int,
     learning_rate: float = 0.001,
+    warmup_epochs: float = 0.5,
+    clip_grads: float = None,
     random_seed: int = 42,
 ) -> Dict[int, float]:
     """Training loop for transformer decoder."""
@@ -119,9 +135,18 @@ def train(
     model.to(device)
     model.train()
 
-    optimizer = Adam(model.parameters(), lr=learning_rate, betas=(0.9, 0.98), eps=1e-9)
+    optimizer = Adam(model.parameters(), lr=learning_rate)
+    n_epoch_steps = len(sequence_data)
+    n_warmup_steps = math.floor(warmup_epochs * n_epoch_steps)
+    n_steps = n_epochs * n_epoch_steps
+    learning_rate_scheduler = LambdaLR(
+        optimizer,
+        partial(cosine_warmup_schedule, warmup_steps=n_warmup_steps, max_steps=n_steps)
+    )
     loss_func = CrossEntropyLoss(ignore_index=PAD_TOKEN_IDX)
     train_loss: Dict[int, float] = {}
+
+    print(f"number of warmup steps: {n_warmup_steps} / {n_steps}")
 
     for epoch in range(1, n_epochs+1):
         # use random batch of sequences over iterating over all possible batches
@@ -134,13 +159,18 @@ def train(
 
             optimizer.zero_grad()
             loss.backward()
-            clip_grad_norm_(model.parameters(), 0.5)
+            if clip_grads:
+                clip_grad_norm_(model.parameters(), clip_grads)
             optimizer.step()
+            learning_rate_scheduler.step()
 
             avg_loss = loss
             train_loss[epoch] = avg_loss.item()
+            current_lr = learning_rate_scheduler.get_last_lr()[0]
 
-            pbar.set_description(f"epoch {epoch} current loss = {avg_loss:.4f}")
+            pbar.set_description(
+                f"epoch {epoch} current loss = {avg_loss:.4f} (LR = {current_lr:.8f})"
+            )
 
         timestamp = datetime.now().isoformat(timespec="seconds")
         print(f"{timestamp} epoch {epoch} loss: {train_loss[epoch]:.4f}")
@@ -187,14 +217,17 @@ if __name__ == "__main__":
 
     SIZE_EMBED = 256
 
-    N_EPOCHS = 5
-    BATCH_SIZE = 256
-    SEQUENCE_LENGTH = 40
-    LEARNING_RATE = 0.001
+    N_EPOCHS = 20
+    BATCH_SIZE = 32
+    SEQ_LEN = 40
+    MIN_WORD_FREQ = 2
+    MAXIMUM_LEARNING_RATE = 0.001
+    WARMUP_EPOCHS = 0.5
+    GRADIENT_CLIP = 5
 
     print("-- training model --")
 
-    data = FilmReviewSequences(split="all", sequence_length=SEQUENCE_LENGTH)
+    data = FilmReviewSequences(split="all", seq_len=SEQ_LEN, min_freq=MIN_WORD_FREQ)
     data_loader = DataLoader(
         data,
         batch_size=BATCH_SIZE,
@@ -203,7 +236,14 @@ if __name__ == "__main__":
         collate_fn=pad_seq2seq_data
     )
     model = NextWordPredictionTransformer(data.vocab_size, SIZE_EMBED)
-    train_loss = train(model, data_loader, N_EPOCHS, LEARNING_RATE)
+    train_loss = train(
+        model,
+        data_loader,
+        N_EPOCHS,
+        MAXIMUM_LEARNING_RATE,
+        WARMUP_EPOCHS,
+        GRADIENT_CLIP
+    )
     save_model(model, name=MODEL_NAME, loss=train_loss[N_EPOCHS])
 
     # generate text
@@ -217,6 +257,6 @@ if __name__ == "__main__":
     print("\n-- generating text --")
 
     tokenizer = IMDBTokenizer()
-    model: NextWordPredictionTransformer = load_model(MODEL_NAME)
+    model: NextWordPredictionTransformer = load_model(MODEL_NAME, latest=True)
     new_text = generate(model, PROMPT, tokenizer, temperature=TEMPERATURE)
     print(new_text)
